a32320693e629595d3148887731462ae2f0debe9
kryspy-loris <91410740+kryspy-loris@users.noreply.github.com>
2022-01-31 18:38:32 +0400

    Update main3.py

--- main3.py
+++ main3.py
@@ -1,10 +1,11 @@
 import os
 
 import requests
-from PyQt5.QtGui import QPixmap
 import sys
+from PyQt5.QtGui import QPixmap
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import Qt
 
 SCREEN_SIZE = [600, 600]
 
@@ -14,15 +15,24 @@ class MyWidget(QMainWindow):
         super().__init__()
         self.start_window()
         self.pushButton.clicked.connect(self.Img)
+        self.zoom_a = 0
 
     def start_window(self):
         uic.loadUi('ui.ui', self)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.zoom_a += 1
+            self.Img()
+        elif event.key() == Qt.Key_PageDown:
+            self.zoom_a -= 1
+            self.Img()
 
     def Img(self):
         latitude = self.latitude.text()
         longitude = self.longitude.text()
         zoom = self.zoom.text()
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll={latitude},{longitude}&spn={zoom},{zoom}&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={latitude},{longitude}&spn={int(zoom) + self.zoom_a},{int(zoom) + self.zoom_a}&l=map"
         response = requests.get(map_request)
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
